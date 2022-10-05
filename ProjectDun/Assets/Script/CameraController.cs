a342d4ac5c0913894d1a9c0d1b0825afc9af199f
Septakuu <septak12345@gmail.com>
2022-10-06 02:10:16 +0900

    Cam Control, Sample Animation

---
+++ ProjectDun/Assets/Script/CameraController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float xmove = 0;  // X축 누적 이동량
+    public float ymove = 0;  // Y축 누적 이동량
+    public float distance = 3;
+    public float zoomSpeed=2;
+    [SerializeField] float m_MaxRayDist = 1;
+    [SerializeField] float m_Zoom = 3f;
+    RaycastHit m_Hit;
+
+    Camera cam;
+	private void Start()
+	{
+        cam = Camera.main;
+        cam.transform.rotation = Quaternion.Euler(45, 0, 0);
+	}
+	void Update()
+    {
+        Zoom();
+        CamRotate();
+    }
+    void CamRotate()
+	{
+        //  마우스 우클릭 중에만 카메라 무빙 적용
+
+        if (Input.GetMouseButton(1))
+        {
+            xmove += Input.GetAxis("Mouse X"); // 마우스의 좌우 이동량을 xmove 에 누적합니다.
+            ymove -= Input.GetAxis("Mouse Y"); // 마우스의 상하 이동량을 ymove 에 누적합니다.
+            cam.transform.rotation = Quaternion.Euler(ymove, xmove, 0); // 이동량에 따라 카메라의 바라보는 방향을 조정합니다.
+        }
+        Vector3 reverseDistance = new Vector3(0.0f, 0.0f, distance); // 카메라가 바라보는 앞방향은 Z 축입니다. 이동량에 따른 Z 축방향의 벡터를 구합니다.
+        cam.transform.position = transform.position - cam.transform.rotation * reverseDistance; // 플레이어의 위치에서 카메라가 바라보는 방향에 벡터값을 적용한 상대 좌표를 차감합니다.
+    }
+    void Zoom()
+	{
+        float scroll = Input.GetAxis("Mouse ScrollWheel")* zoomSpeed;
+        distance = Mathf.Clamp(distance -scroll , 3, 10);
+    }
+
+}
---
+++ ProjectDun/Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[RequireComponent(typeof(Stat))]
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMovement : MonoBehaviour
+{
+    public enum STATE
+    {
+        Idle,
+        Move,
+        Chase,
+        Attack,
+        Dead,
+    }
+
+    NavMeshAgent agent;
+    Vector3 movePoint;
+
+    Camera cam;
+    Rigidbody rigid;
+    Animator anim;
+
+    RaycastHit hit;                      // 마우스 동작으로 리턴된 정보가 저장 될 변수
+    Damagable target;               // 공격 시 대상.
+    Stat stat;
+    float playerY => transform.position.y;
+    Coroutine attackCoroutine;
+
+    [SerializeField] STATE state;
+    [SerializeField] LayerMask enemyLayer;
+    [SerializeField] float attackRange;
+    private void Start()
+    {
+        state = STATE.Idle;
+        cam = Camera.main;
+        rigid = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        stat = GetComponent<PlayerStat>();
+        anim = GetComponentInChildren<Animator>();
+    }
+    private void Update()
+    {
+        StateChanger();
+        switch (state)
+        {
+            case STATE.Idle:
+                Idle();
+                break;
+            case STATE.Move:
+                Move();
+                break;
+            case STATE.Chase:
+                Chase();
+                break;
+            case STATE.Attack:
+                Attack();
+                break;
+            case STATE.Dead:
+                break;
+        }
+    }
+
+    private void StateChanger()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray point = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(point, out hit, float.MaxValue))
+            {
+                target = hit.collider.GetComponent<Damagable>();
+                if (hit.collider.gameObject.tag == "Ground")
+                    state = STATE.Move;
+
+                if(target != null)              // target 이  null이 아니고,
+				{
+                    state = STATE.Chase;
+                    // attackRange안의 적의 collider[]을 받아와서,
+                    // target의 instanceid와 대조시켜 올바른 대상을 공격
+                    // 또는 target과 나 사이의 distance가 attackrange보다 작거나 같다면!
+
+                }
+            }
+        }
+    }
+    private void Idle()
+	{
+        anim.SetBool("isMove", false);
+	}
+    private void Chase()
+	{
+        agent.SetDestination(target.transform.position);
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+        bool attackable = distance < attackRange;
+        if (attackable)
+            state = STATE.Attack;
+    }
+    private void Move()
+    {
+        movePoint = hit.point;
+        movePoint.y = transform.position.y;
+        agent.SetDestination(movePoint);
+        anim.SetBool("isMove", true);
+        if (agent.hasPath && agent.remainingDistance <= 0.1f)
+            state = STATE.Idle;
+    }
+    private void Attack()
+	{
+        agent.SetDestination(transform.position);
+
+        if (attackCoroutine != null)
+            return;
+
+        attackCoroutine = StartCoroutine(AttackCycle());
+    }
+    IEnumerator AttackCycle()
+	{
+        WaitForSeconds wait = new WaitForSeconds(stat.attackRate);
+		while (state == STATE.Attack)
+		{
+            anim.SetBool("isMove", false);
+            anim.SetBool("isAttack",true);
+            yield return wait;
+		}
+        StopCoroutine(attackCoroutine);
+        attackCoroutine = null;
+        anim.SetBool("isAttack", false);
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Handles.color = Color.green;
+        GUIStyle style = new GUIStyle() { fontSize = 30 };
+        Handles.Label(transform.position, state.ToString());
+    }
+#endif
+}
+
+
+
+
